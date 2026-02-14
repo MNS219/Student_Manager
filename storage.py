25cedd92028bf85f5d27a8e88f20e66b3612beea
Sandeep D S <sandeepds7273@gmail.com>
2026-02-14 21:06:46 +0530

    feat: added search_student function

--- storage.py
+++ storage.py
@@ -7,3 +7,9 @@ def save_student(student):
 def get_students():
     with open(FILE, "r") as f:
         return f.readlines()
+
+def search_student(roll):
+    students = get_students()
+    for s in students:
+        if s.startswith(roll):
+            print("Found:", s)
