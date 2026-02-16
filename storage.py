a086f6b45ad4799747bbc3074c88b704ee8194f1
Vansh-1007 <157832429+Vansh-1007@users.noreply.github.com>
2026-02-16 19:50:01 +0530

    feature:Delete Student

--- storage.py
+++ storage.py
@@ -7,3 +7,8 @@ def save_student(student):
 def get_students():
     with open(FILE, "r") as f:
         return f.readlines()
+    
+def delete_student(roll):
+    students = get_students()
+    new = [s for s in students if not s.startswith(roll)]
+    open(FILE, "w").writelines(new)
